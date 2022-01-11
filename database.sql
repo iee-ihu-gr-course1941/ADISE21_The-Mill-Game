f13eac37f5681efd7c2b8bb6468d125e68a3c0e0
Igneez <mixalisol@hotmail.com>
2022-01-11 12:01:18 +0200

    vasi pieces

--- database.sql
+++ database.sql
@@ -1,31 +1,139 @@
 
+DROP TABLE IF EXISTS `board`;
+DROP TABLE IF EXISTS `boardempty`;
+DROP TABLE IF EXISTS `game_status`;
+DROP TABLE IF EXISTS `players`;
+
 
 CREATE TABLE `board` ( 
-`x` tinyint(1) NOT NULL, 
+`x` varchar(1) NOT NULL, 
 `y` tinyint(1) NOT NULL,
 `piece_color` enum('W','B') DEFAULT NULL,
-`Boardslot` enum('T','F'), 
- PRIMARY KEY (`x`,`y`) );
+`boardslot` enum('T','F') NOT NULL, 
+ PRIMARY KEY (`x`,`y`) )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `boardempty`;
 
 CREATE TABLE `boardempty` (
-`x` tinyint(1) NOT NULL, 
+`x` varchar(1) NOT NULL, 
 `y` tinyint(1) NOT NULL,
 `piece_color` enum('W','B') DEFAULT NULL,
-`boardslot` enum('T','F'), 
- PRIMARY KEY (`x`,`y`) );
+`boardslot` enum('T','F')NOT NULL, 
+ PRIMARY KEY (`x`,`y`) )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `players` ( 
 `username` varchar(20) DEFAULT NULL, 
 `piece_color` enum('B','W') NOT NULL,
 `token` varchar(40) DEFAULT NULL,
-PRIMARY KEY (`token`) )
+PRIMARY KEY (`piece_color`) )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `game_status` (
 `status` enum('not active','initialized','started','ended','aborded') NOT NULL DEFAULT 'not active',
 `p_turn` enum('W','B') DEFAULT NULL,
 `result` enum('B','W','D') DEFAULT NULL,
-`last_change` timestamp NULL DEFAULT NULL )
+`last_change` timestamp NULL DEFAULT NULL )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+INSERT INTO `board` (`x`, `y`, `piece_color`, `boardslot`) VALUES 
+('A',1,NULL,'T'),
+('A',2,NULL,'F');
+('A',3,NULL,'F'),
+('A',4,NULL,'T'),
+('A',5,NULL,'F'),
+('A',6,NULL,'F'),
+('A',7,NULL,'T'),
+('B',1,NULL,'F'),
+('B',2,NULL,'T'),
+('B',3,NULL,'F'),
+('B',4,NULL,'T'),
+('B',5,NULL,'F'),
+('B',6,NULL,'T'),
+('B',7,NULL,'F'),
+('C',1,NULL,'F'),
+('C',2,NULL,'F'),
+('C',3,NULL,'T'),
+('C',4,NULL,'T'),
+('C',5,NULL,'T'),
+('C',6,NULL,'F'),
+('C',7,NULL,'F'),
+('D',1,NULL,'T'),
+('D',2,NULL,'T'),
+('D',3,NULL,'T'),
+('D',4,NULL,'F'),
+('D',5,NULL,'T'),
+('D',6,NULL,'T'),
+('D',7,NULL,'T'),
+('E',1,NULL,'F'),
+('E',2,NULL,'F'),
+('E',3,NULL,'T'),
+('E',4,NULL,'T'),
+('E',5,NULL,'T'),
+('E',6,NULL,'F'),
+('E',7,NULL,'F'),
+('F',1,NULL,'F'),
+('F',2,NULL,'T'),
+('F',3,NULL,'F'),
+('F',4,NULL,'T'),
+('F',5,NULL,'F'),
+('F',6,NULL,'T'),
+('F',7,NULL,'F'),
+('G',1,NULL,'T'),
+('G',2,NULL,'F'),
+('G',3,NULL,'F'),
+('G',4,NULL,'T'),
+('G',5,NULL,'F'),
+('G',6,NULL,'F'),
+('G',7,NULL,'T');
+
+
+
+INSERT INTO `boardempty` (`x`, `y`, `piece_color`, `boardslot`) VALUES 
+('A',1,NULL,'T'),
+('A',2,NULL,'F');
+('A',3,NULL,'F'),
+('A',4,NULL,'T'),
+('A',5,NULL,'F'),
+('A',6,NULL,'F'),
+('A',7,NULL,'T'),
+('B',1,NULL,'F'),
+('B',2,NULL,'T'),
+('B',3,NULL,'F'),
+('B',4,NULL,'T'),
+('B',5,NULL,'F'),
+('B',6,NULL,'T'),
+('B',7,NULL,'F'),
+('C',1,NULL,'F'),
+('C',2,NULL,'F'),
+('C',3,NULL,'T'),
+('C',4,NULL,'T'),
+('C',5,NULL,'T'),
+('C',6,NULL,'F'),
+('C',7,NULL,'F'),
+('D',1,NULL,'T'),
+('D',2,NULL,'T'),
+('D',3,NULL,'T'),
+('D',4,NULL,'F'),
+('D',5,NULL,'T'),
+('D',6,NULL,'T'),
+('D',7,NULL,'T'),
+('E',1,NULL,'F'),
+('E',2,NULL,'F'),
+('E',3,NULL,'T'),
+('E',4,NULL,'T'),
+('E',5,NULL,'T'),
+('E',6,NULL,'F'),
+('E',7,NULL,'F'),
+('F',1,NULL,'F'),
+('F',2,NULL,'T'),
+('F',3,NULL,'F'),
+('F',4,NULL,'T'),
+('F',5,NULL,'F'),
+('F',6,NULL,'T'),
+('F',7,NULL,'F'),
+('G',1,NULL,'T'),
+('G',2,NULL,'F'),
+('G',3,NULL,'F'),
+('G',4,NULL,'T'),
+('G',5,NULL,'F'),
+('G',6,NULL,'F'),
+('G',7,NULL,'T');
